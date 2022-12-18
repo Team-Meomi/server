a9c59a48fcb9c754970b22ed633040395affd330
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2022-12-18 11:06:59 +0900

    :sparkles: :: 컨퍼런스 기능 컨트롤러 추가

--- src/main/kotlin/com/project/meomi/domain/conference/presentation/ConferenceController.kt
+++ src/main/kotlin/com/project/meomi/domain/conference/presentation/ConferenceController.kt
@@ -1,4 +1,76 @@
 package com.project.meomi.domain.conference.presentation
 
-class ConferenceController {
+import com.project.meomi.domain.conference.presentation.data.request.CreateConferenceRequest
+import com.project.meomi.domain.conference.presentation.data.request.UpdateConferenceRequest
+import com.project.meomi.domain.conference.presentation.data.response.ConferenceInfoResponse
+import com.project.meomi.domain.conference.presentation.data.response.ConferencePeopleResponse
+import com.project.meomi.domain.conference.presentation.data.response.ConferenceResponse
+import com.project.meomi.domain.conference.service.ConferenceQueryService
+import com.project.meomi.domain.conference.service.ConferenceService
+import com.project.meomi.domain.conference.utils.ConferenceConverter
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("api/v1/user/conference/")
+class ConferenceController(
+    private val conferenceConverter: ConferenceConverter,
+    private val conferenceService: ConferenceService,
+    private val conferenceQueryService: ConferenceQueryService
+) {
+
+    @PostMapping
+    fun createConference(@RequestBody @Valid request: CreateConferenceRequest): ResponseEntity<Void> =
+        conferenceConverter.toDto(request)
+            .let { conferenceService.createConference(it) }
+            .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+
+    @PatchMapping("{id}")
+    fun updateConference(@PathVariable id: Long, @RequestBody @Valid request: UpdateConferenceRequest): ResponseEntity<Void> =
+        conferenceConverter.toDto(request, id)
+            .let { conferenceService.updateConference(it) }
+            .let { return ResponseEntity.ok().build() }
+
+    @DeleteMapping("{id}")
+    fun deleteConference(@PathVariable id: Long): ResponseEntity<Void> =
+        conferenceConverter.toDto(id)
+            .let { conferenceService.deleteConference(it) }
+            .let { ResponseEntity.ok().build() }
+
+    @GetMapping("{id}")
+    fun findConferenceById(@PathVariable id: Long): ResponseEntity<ConferenceResponse> =
+        conferenceConverter.toDto(id)
+            .let { conferenceQueryService.findConferenceById(it) }
+            .let { conferenceConverter.toResponse(it) }
+            .let { ResponseEntity.ok(it) }
+
+    @PostMapping("{id}")
+    fun joinConference(@PathVariable id: Long): ResponseEntity<Void> =
+        conferenceConverter.toDto(id)
+            .let { conferenceService.joinConference(it) }
+            .let { ResponseEntity.ok().build() }
+
+    @DeleteMapping("cancel/{id}")
+    fun cancelConference(@PathVariable id: Long): ResponseEntity<Void> =
+        conferenceConverter.toDto(id)
+            .let { conferenceService.cancelConference(it) }
+            .let { ResponseEntity.ok().build() }
+
+    @GetMapping("info/{id}")
+    fun findConferenceInfo(@PathVariable id: Long): ResponseEntity<ConferenceInfoResponse> =
+        conferenceConverter.toDto(id)
+            .let { conferenceQueryService.findConferenceInfo(it) }
+            .let { conferenceConverter.toResponse(it) }
+            .let { ResponseEntity.ok(it) }
+
+    @GetMapping("people/{id}")
+    fun findConferencePeople(@PathVariable id: Long): ResponseEntity<ConferencePeopleResponse> =
+        conferenceConverter.toDto(id)
+            .let { conferenceQueryService.findConferencePeople(it) }
+            .let { conferenceConverter.toResponse(it) }
+            .let { ResponseEntity.ok(it) }
+
 }
