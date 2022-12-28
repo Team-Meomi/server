4a198aa56bf1f093c6362b41051a1b2b8d799115
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2022-12-28 20:27:27 +0900

    :sparkles: :: timeZone 서울로 설정 추가

--- src/main/kotlin/com/project/meomi/MeomiApplication.kt
+++ src/main/kotlin/com/project/meomi/MeomiApplication.kt
@@ -4,12 +4,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
+import javax.annotation.PostConstruct
 
 @EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class MeomiApplication
 
+@PostConstruct
+fun timeZone() {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+}
 fun main(args: Array<String>) {
 	runApplication<MeomiApplication>(*args)
 }
